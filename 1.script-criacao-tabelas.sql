e2460c1937f882979db710ea4cc757cbe3211163
negraop <pedro.negrao12@gmail.com>
2023-12-01 01:06:09 -0300

    new fix querys de create table

--- 1.script-criacao-tabelas.sql
+++ 1.script-criacao-tabelas.sql
@@ -52,11 +52,14 @@ CREATE TABLE tb_plano_tratamento (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   descricao VARCHAR(500) NOT NULL,
   id_dentista INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_tratamento_dentista FOREIGN KEY (id_dentista) REFERENCES tb_dentista (id)
+  id_paciente INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_tratamento_dentista FOREIGN KEY (id_dentista) REFERENCES tb_dentista (id),
+  CONSTRAINT fk_tratamento_paciente FOREIGN KEY (id_paciente) REFERENCES tb_paciente (id)
 );
 
 CREATE TABLE tb_anamnese (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id_dentista INT UNSIGNED NOT NULL,
   id_paciente INT UNSIGNED NOT NULL,
   pergunta_1 VARCHAR(255),
   pergunta_2 VARCHAR(255),
@@ -68,5 +71,6 @@ CREATE TABLE tb_anamnese (
   dente_3 VARCHAR(255),
   dente_etc VARCHAR(255),
   dente_32 VARCHAR(255),
+  CONSTRAINT fk_anamnese_dentista FOREIGN KEY (id_dentista) REFERENCES tb_dentista (id),
   CONSTRAINT fk_anamnese_paciente FOREIGN KEY (id_paciente) REFERENCES tb_paciente (id)
 );
